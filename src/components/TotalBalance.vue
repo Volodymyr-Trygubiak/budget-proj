b28c405952d2b04661c17cec88b05da9bbc15cac
Volodymyr Trygubiak <trigubiak@gmail.com>
2021-03-10 16:17:53 +0200

    added color for  total balance

--- src/components/TotalBalance.vue
+++ src/components/TotalBalance.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="total-value">Balance: {{ total }}</div>
+  <div class="total-value" :class="totalColor">Balance: {{ total }}</div>
 </template>
 
 <script>
@@ -8,9 +8,21 @@ export default {
   props: {
     total: {
       type: Number,
-      default: 0
-    }
-  }
+      default: 0,
+    },
+  },
+
+  computed: {
+    totalColor() {
+      if (this.total > 0) {
+        return "more";
+      }if (this.total < 0) {
+        return "less";
+      } else {
+        return '';
+      }
+    },
+  },
 };
 </script>
 
@@ -20,5 +32,11 @@ export default {
   text-transform: uppercase;
   padding: 20px;
   text-align: center;
+}
+.more {
+  color: #1cca47;
+}
+.less {
+  color: #ff0000;
 }
 </style>
